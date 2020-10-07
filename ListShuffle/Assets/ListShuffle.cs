c8b8400921f5c54ded8070bcfad33da5cddd9c69
KotaOishi <k.oishi@pr.cri.co.jp>
2020-10-07 20:21:33 +0900

    コミット

---
+++ ListShuffle/Assets/ListShuffle.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+public static class ListShuffle
+{
+    /// <summary>
+    /// 渡されたリストの中身をシャッフルして返す
+    /// </summary>
+    /// <param name="list">リスト</param>
+    /// <returns>シャッフル後のリスト</returns>
+    public static List<int> GetListShuffle( List<int> list )
+    {
+        return list.OrderBy( v => System.Guid.NewGuid() ).ToList();  
+    }
+}
---
+++ ListShuffle/Assets/TestCode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TestCode : MonoBehaviour
+{
+    List<int> list = new List<int>{ 1, 2, 3, 4, 5 };
+
+    /// <summary>
+    /// リストのシャッフルをおこないそれをログに表示する
+    /// </summary>
+    void Start()
+    {
+        var afterList = ListShuffle.GetListShuffle( list );
+        afterList.ForEach( v => Debug.Log( v ) ); 
+    }
+}
